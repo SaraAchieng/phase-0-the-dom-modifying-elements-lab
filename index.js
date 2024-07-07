316907bbd56ef3838d0ff9d9564f0a4d6a1aba67
Sara Achieng <achieng997@gmail.com>
2024-07-07 22:31:11 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const node = document.getElementById("main");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = 'Sara is the champion';
